24e7f9f645c1ece13d9bc801a0f7497d28a76915
Nanthakarn Limkool <nanthakarn18@gmail.com>
2020-01-20 00:22:29 +0700

    hello.java
    self review Sun 20th Jun

--- week1/Hello.java
+++ week1/Hello.java
@@ -1,36 +1,47 @@
-/** A really boring class.name
+/**
+ * A really boring class.name
  */
 public class Hello {
-    public static void main(String[] args) { // String[] = array of Strings
-      // this is an end of line comment
+  /**
+   * Run the appication.
+   * 
+   * @param arg - commant line parameter, first one is assumed to be person's name
+   *            greet.
+   */
+  public static void main(String[] args) { // String[] = array of Strings
+    // this is an end of line comment
 
-      String x = "g'day";
-      // TODO
-      // IF there is a command line arguments (strings)
-      // first argument as name of person to greet.
-      // If no command line argument, then print "Hello, Coder"
-      if (args.length > 0) {
-        String name = args[0];
-      }
-      else {
-        String name = "Coder";
-      }
-      // conditional assignment
-      String name = (args.length)
-      // there is no variable name (out of scope)
-      System.out.println("Hello World," + name); /* I hope so */
-
-      // Print all of the command line arguments (strings)
-      // This is like Python:
-      // for s in args:
-      //    print(s)name
-      System.out.println("Argument are:");
-      for (String s : args) {
-        System.out.println(s);
-      }
-
-      public static void name() {
-        
-      }
+    String x = "g'day"; // local variables
+    // TODO
+    // IF there is a command line arguments (strings), then use the
+    // first argument as name of person to greet.
+    // If no command line argument, then print "Hello, Coder"
+    String name;
+    if (args.length > 0) {
+      name = args[0];
+    } else {
+      name = "Coder";
     }
+    // conditional assignment
+    // String name = (args.length > 0) ? args[0] : "coder";
+
+    System.out.println("Hello World, " + name); /* I hope so */
+
+    // Print all of the command line arguments (strings)
+    // This is like Python:
+    // for s in args:
+    // print(s)name
+    System.out.println("Argument are:");
+    for (String s : args) {
+      System.out.println(s);
+    }
+    // เขียนแบบนี้ก้ได้ for (String s : args) System.out.println(s);
+    // ถ้ามีบรรทัดเดียว
   }
+
+  public static void method2() {
+
+    String name = "Kasetsart U."; // another local variable
+
+  }
+}
