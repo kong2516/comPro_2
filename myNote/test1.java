b9406dc7b436a720cb581e1fff7710f083043936
Nanthakarn Limkool <nanthakarn18@gmail.com>
2020-01-28 12:52:56 +0700

    Create test1.java

---
+++ myNote/test1.java
@@ -0,0 +1,14 @@
+public class test1 {
+
+    public static void main(String[] args) {
+        String[] a = { "cat", "dog" };
+        String[] b = { "ant", "bat", "fish" };
+
+        b = a;
+
+        b[0] = "dd";
+        a[1] = "cc";
+
+        System.out.println(b[0] + " ," + b[1]);
+    }
+}
